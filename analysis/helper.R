535414d3ad346efe604b0c3b469e08e9492e9eff
George Whittington <lwhittington2231@gmail.com>
2025-11-15 22:21:59 -0500

    Update helper.R Fixed date handeler to convert multiple types of date formats Created a write function to create long versions of the datasets

--- analysis/helper.R
+++ analysis/helper.R
@@ -1,6 +1,8 @@
 # Programmer: George Whittington
 # Date: Purpose: Defied functions to help in data exploration
 
+i_am("analysis/helper.R")
+
 #' Fix EPA Date Formatting
 #'
 #' Converts a column containing "mm/dd/yy" strings into proper R Date objects.
@@ -12,7 +14,49 @@
 #' @return A tibble or dataframe with the specified column transformed to Date format.
 #' @export
 convert_to_date <- function(data, col_name = "Date") {
-  data[[col_name]] <- lubridate::mdy(data[[col_name]])
+  # the Date columns are in different formats across datasets
+  data |> 
+    mutate(
+      Date = parse_date_time(Date, orders = c("mdy", "mdy HM"))
+    )
+}
 
-  return(data)
+#' Convert EPA Data to Long Format with Uncertainty and Write to CSV
+#' 
+#' Takes both a concentration and uncertainty file and pivots them to a long format
+#' and combines then into one csv to be further analyzed 
+#' 
+#' @param data_con A tibble or dataframe containing the concentration data
+#' @param data_unc A tibble or dataframe containing the uncertainty data
+#' 
+#' @return NUll. It creates a combined csv file
+#' @export
+write_long_data <- function(data_con, data_unc) {
+  # keep the data column as it and move the names and values of each component to a row
+  data_con_long <- data_con |> 
+    pivot_longer(
+      -Date,
+      names_to = "Component",
+      values_to = "Concentration"
+    )
+  
+  # keep the data column as it and move the names and values of each component to a row
+  data_unc_long <- data_unc |> 
+    pivot_longer(
+      -Date,
+      names_to = "Component",
+      values_to = "Uncertainty"
+    )
+  
+  # join them together and add uncertainty percentage
+  combined <- data_con_long |> 
+    full_join(
+      data_unc_long,
+      join_by(Date, Component)
+    ) |> 
+      mutate(
+        Uncertainty_Ratio = Uncertainty / Concentration
+      )
+  
+  write_csv(combined, here("data/"))
 }
